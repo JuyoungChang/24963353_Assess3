1c95969fb29dd8be2a9caba0d878362b1a99cfe6
JuyoungChang <juyoung.chang@student.uts.edu.au>
2024-09-25 20:05:50 +1000

    adf

--- Assets/Script/PacStudent Movement.cs
+++ Assets/Script/PacStudent Movement.cs
@@ -5,19 +5,28 @@ using UnityEditor.Experimental.GraphView;
 using Vector3 = UnityEngine.Vector3;
 using Vector2 = UnityEngine.Vector3;
 using UnityEngine;
+using Unity.VisualScripting;
+using System.ComponentModel;
 
 public class PacStudentMovement : MonoBehaviour
 {
     public float movingSpeed = 0.2f;
     public  AudioSource movingMan;//chunjat!
-    public Vector2[] coordinates = {
+    public AudioClip clip;
+    public Vector2[] coordinates = 
+    {
         new Vector2(-10.5f, -0.5f), new Vector2(-10.5f, 3.5f), new Vector2(-5.5f, 3.5f), new Vector2(-5.5f, -0.5f)
     };
     private Vector2 target;
+    private float time = 0f;
+    private int point = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(coordinates.Length > 0){
+            target = coordinates[0];
+        }
         
     }
 
@@ -26,6 +35,19 @@ public class PacStudentMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        time += Time.deltaTime * movingSpeed / Vector2.Distance(transform.position, target);
+        transform.position = Vector2.Lerp(transform.position, target, time);
+
+        if(Vector2.Distance(transform.position, target) < 0.1f)
+        {
+            point++;
+            if(point >= coordinates.Length)
+            {
+                point = 0;
+            }
+            target = coordinates[point];
+            time = 0f;
+        }
+
     }
 }
