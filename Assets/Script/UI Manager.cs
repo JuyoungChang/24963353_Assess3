b9b618a986627e95bde2d52108544abc4237b288
JuyoungChang <juyoung.chang@student.uts.edu.au>
2024-10-11 14:46:49 +1100

    edit

--- Assets/Script/UI Manager.cs
+++ Assets/Script/UI Manager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneLoadingButton : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int score;
+    private Text ScoreText;
+    private float timer;
     void Start()
     {
-        
+        ScoreText.text = "Score:" + 0;
     }
 
     // Update is called once per frame
@@ -19,5 +23,9 @@ public class SceneLoadingButton : MonoBehaviour
     public void LoadLv1()
     {
         SceneManager.LoadScene("Level1");
+    }
+    public void ScoreAdd()
+    {
+        score++;
     }
 }
