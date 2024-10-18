ff4ac5ab4c02c63c6c1250966e9f6d0d8f6ee38c
JuyoungChang <juyoung.chang@student.uts.edu.au>
2024-10-18 12:47:55 +1100

    movement

--- Assets/Script/PacStudentController.cs
+++ Assets/Script/PacStudentController.cs
@@ -1,29 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-    private Vector2 start;
-    private Vector2 target;
-    public float speed = 3f;
-    private string lastInput;
+    public bool isLerping = false;
+    public Vector2 lastInput = Vector2.zero;
+    public Vector2 currentINput = Vector2.zero;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        start = transform.position;
-        target = start;
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputMan();
     }
 
-    void Movement()
+    void InputMan()
     {
-        transform.position = Vector2.Lerp(start, target, speed * Time.deltaTime);
+        if(Input.GetKeyDown("w") && !isLerping)
+        {
+            lastInput = Vector2.up;
+            StartMove();
+        }
+        if(Input.GetKeyDown("s") && !isLerping)
+        {
+           lastInput = Vector2.down;
+           StartMove();
+        }
+        if(Input.GetKeyDown("a") && !isLerping)
+        {
+           lastInput = Vector2.left;
+           StartMove();
+        }
+        if(Input.GetKeyDown("d") && !isLerping)
+        {
+           lastInput = Vector2.right;
+           StartMove();
+        }
+    }
+
+    private void StartMove()
+    {
+        Vector2 position = transform.position;
+        Vector2 targetPosition = position + lastInput;
+        StartCoroutine(Movement(targetPosition));
+    }
+    private IEnumerator Movement(Vector2 target)
+    {
+        isLerping = true;
+        Vector2 start = transform.position;
+        //Vector2 target = start + direction;
+        float elapsed = 0;
+        //float duration = 0.01f;
+        while(elapsed < 1f )
+        {
+            transform.position = Vector2.Lerp(start, target, elapsed);
+            elapsed += Time.deltaTime * 5f;
+            yield return null;  
+        }
+        transform.position = target;
+        isLerping =false;
     }
 
 }
